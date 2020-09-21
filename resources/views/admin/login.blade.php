67c13543bc991c57230439ed008f9d55f1acb167
philldeveloper <philldeveloper@gmail.com>
2020-09-21 16:52:32 -0300

    add login page admin reestilized

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.admin')
+@extends('layouts.app')
 
 <style>
 .container#login{
@@ -6,21 +6,29 @@
     max-width: 900px !important;
     padding-top: 30px;  
 }
+
+.card-content-custom{
+    /* border-top: 20px solid !important;  */
+    border: 1px solid;
+    border-image: linear-gradient(to left, #B06AB3, #4568DC) 1 !important;
+    border-radius: 0;
+}
+
 </style>
 
 @section('content')
 <div class="container main-font" id="login">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <h1 class="text-light text-center main-font display-4 py-4">Array</h1>
-            <div  class="text-light text-center">ACESSO ADMINISTRATIVO</div>
-            <div class="card border-0">
-                <div class="card-body bg-too-dark border-0">
+        <div class="col-md-7">
+            <h1 class="text-light text-center main-font py-5">Acesso administrativo</h1>
+            <div class="card rounded-0 shadow">
+                <div class="card-body card-content-custom rounded-0 bg-too-dark shadow p-4">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-                        
+
                         <div class="form-group row">
-                            <div class="col-md-9 mx-auto mt-4">
+                            <div class="col-md-9 mx-auto py-2 mt-3">
+                                <label for="#" class="text-light">Email:</label>
                                 <input placeholder="Insira seu email" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -33,6 +41,7 @@
 
                         <div class="form-group row">
                             <div class="col-md-9 mx-auto">
+                                <label for="#" class="text-light">Senha:</label>
                                 <input placeholder="Insira sua senha" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -42,12 +51,18 @@
                                 @enderror
                             </div>
                         </div>
-
+                      
                         <div class="form-group row py-4">
                             <div class="col-md-9 mx-auto">
                                 <button type="submit" class="btn btn-custom btn-block btn-custom-2" style="max-width: 100%">{{ __('Entrar') }}</button>
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link mt-3" href="{{ route('password.request') }}">
+                                        {{ __('Esqueceu sua senha?') }}
+                                    </a>
+                                 @endif
                             </div>
                         </div>
+
                     </form>
                 </div>
             </div>
